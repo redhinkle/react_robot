69d67b6c89374e576d35ab1908592046ecd6019f
Jared Hinkle <jared.h.hinkle@gmail.com>
2020-07-18 14:11:29 -0700

    first commit

---
+++ build/precache-manifest.ad726a740ea8c8e94eb15c1004fe9f36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95c692919c222dfc1b4bb63cb4e53cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "543c9ec80a80c813e414",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e87fe15682e6f7094f9a",
+    "url": "./static/js/2.0849a203.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.0849a203.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543c9ec80a80c813e414",
+    "url": "./static/js/main.6266a103.chunk.js"
+  },
+  {
+    "revision": "13653c7c79b24e3fa405",
+    "url": "./static/js/runtime-main.125a6ab4.js"
+  },
+  {
+    "revision": "ed3e69328073eddb019653925da99447",
+    "url": "./static/media/Robots-Square.ed3e6932.jpg"
+  }
+]);
